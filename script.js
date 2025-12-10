d9736fa143cb84437c8def423f7a19756a99fdd7
ARkhan214 <abdurrahimkhan214dc@gmail.com>
2025-12-11 00:55:05 +0600

    add
    Final mode

--- script.js
+++ script.js
@@ -2,19 +2,32 @@ const qrcodeUrl = document.getElementById("qrcode_url");
 const qrcoButton = document.getElementById("qrcode_button");
 const qrcodeImage = document.getElementById("qr_img");
 
-qrcoButton.addEventListener("click", async function(){
+qrcoButton.addEventListener("click", async function () {
 
-const dataUrl =await QRCode.toDataURL(qrcodeUrl.value);
-console.log(dataUrl);
-qrcodeImage.src = dataUrl;
+    const dataUrl = await QRCode.toDataURL(qrcodeUrl.value);
+    console.log(dataUrl);
+    qrcodeImage.src = dataUrl;
 
 });
 
 
-// qrcodeButton.addEventListener("click", () => {
-//     QRCode.toDataURL(qrcodeURL.value).then(dataURL => {
-//         console.log(dataURL);
-//         qrcodeImage.src = dataURL;
-//     })
-// });
+// Download QR Code png=======================================================================
+const downloadBtn = document.getElementById("download_btn");
 
+qrcoButton.addEventListener("click", async function () {
+    const dataUrl = await QRCode.toDataURL(qrcodeUrl.value);
+
+    qrcodeImage.src = dataUrl;
+
+    downloadBtn.style.display = "block"; // show download button
+    downloadBtn.setAttribute("data-url", dataUrl); // store url
+});
+
+downloadBtn.addEventListener("click", function () {
+    const url = downloadBtn.getAttribute("data-url");
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "qrcode.png";
+    link.click();
+});
